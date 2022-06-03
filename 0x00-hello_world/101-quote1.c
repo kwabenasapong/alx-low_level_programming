997dc89f2c793d46d092e60df44ae04b342d619b
Kwabena Sapong <kwabenasapong@gmail.com>
2022-06-02 22:53:03 -0700

    update task 8

---
+++ 0x00-hello_world/101-quote1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 69, 1, stderr);
+return (1);
+}
