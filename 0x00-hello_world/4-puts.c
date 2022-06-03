4ef4ee34d1bd726a5a5794cbe7f7a6ab3940d13b
Kwabena Sapong <kwabenasapong@gmail.com>
2022-06-02 20:56:13 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+/*
+ * This program prints out
+ * Programming is like building
+ * a multilingual puzzle
+*/
+#include <stdio.h>
+int main ()
+{
+	puts("\"Programming is a like building a multilingual puzzle");
+	return (0);
+}
