dfb998f901e34722231869220afbe4a5bb3c8edc
Kwabena Sapong <kwabenasapong@gmail.com>
2022-06-02 21:19:11 -0700

    Update Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
-/*
- * This program prints out
- * Programming is like building
- * a multilingual puzzle
-*/
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
