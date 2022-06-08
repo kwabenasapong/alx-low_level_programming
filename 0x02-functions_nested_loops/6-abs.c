08916d157ae9625ed931d653bed4e0cf5fe6adc8
Kwabena Sapong <kwabenasapong@gmail.com>
2022-06-08 07:29:08 -0700

    Tas 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - entry point
+ * @variablename: integer passed to function
+ * Return: Always variablename (success)
+ */
+int _abs(int)
+{
+	int i;
+
+	if (i < 0);
+	{
+		i *= -1;
+	}
+	return (i);
+}
