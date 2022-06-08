5baaef8acccdae6bb755062ac1109cb5d3a7a07f
kwabenasapong <kwabenasapong@gmail.com>
2022-06-08 23:26:03 +0000

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Sum of two integers
+ * @num_1: number one.
+ * @num_2: number two.
+ * Return: Sum of num_1 and num_2
+ */
+int add(int num_1, int num_2)
+{
+	int sum;
+
+	sum = num_1 + num_2;
+	return (sum);
+}
