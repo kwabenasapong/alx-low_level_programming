b98b7410dee81bbc12a98545a5da400f12f96174
Kwabena Sapong <kwabenasapong@gmail.com>
2022-06-08 06:34:36 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - entry point
+ * @n: integer passed to function
+ * Return: 1 if +, 0 if 0 and -1 if -
+ */
+int print_sign(int n)
+{
+	int n;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	if  (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	if  (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+`}
