a35d6cff9133308be30f009438767334068fa9ee
kwabenasapong <kwabenasapong@gmail.com>
2022-06-08 01:17:18 +0000

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * main - entry point
+ * Return: Always 0 (success)
+ */
+int main()
+{
+	char c[] = "_putchar";
+	char i = 0;
+
+	while(1 < 9)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
