7b270c8a23c4e28ee3a59b7f3e5e8de1df01e382
kwabenasapong <kwabenasapong@gmail.com>
2022-06-13 21:16:52 +0000

    Add task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - entry point
+ *
+ * @str: pointer to function
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
