552acc3bbd8b3363481af91f0c85b8b13b085b4b
kwabenasapong <kwabenasapong@gmail.com>
2022-06-14 02:03:25 +0000

    Add task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,17 +9,17 @@
  */
 void print_array(int *a, int n)
 {
-	int i;
+	int j;
 
-	for (i = 0; i < n; i++)
+	for (j = 0; j < n; j++)
 	{
-		if (i != n - 1)
+		if (j != n - 1)
 		{
-			printf("%d, ", a[i]);
+			printf("%d, ", a[j]);
 		}
 		else
 		{
-			printf("%d", a[i]);
+			printf("%d", a[j]);
 		}
 	}
 	printf("\n");
