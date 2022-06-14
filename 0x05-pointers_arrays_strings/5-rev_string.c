690b46045745d151e61f14e6daf1e6091ad9483b
kwabenasapong <kwabenasapong@gmail.com>
2022-06-14 00:35:06 +0000

    Add task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - entry point
+ *
+ * @s: pointer to function
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = 0,
+	char temp;
+
+	while (s[j] != '\0')
+		j++;
+	j--;
+	while (i < j)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+		i++;
+		j--;
+	}
+}
