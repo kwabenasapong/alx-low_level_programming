e39b73140118d5caf650cfd1bb2aff0c7cabee1b
kwabenasapong <kwabenasapong@gmail.com>
2022-06-15 16:41:00 +0000

    Add task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+int _strlen(char *s);
+
+/**
+ * _strcat - entry point
+ *
+ * @dest: pointer to function
+ * @src: pointer function
+ * Return: dest (success)
+ */
+char *_strcat(char *dest, char *src)
+{
+	int l = _strlen(dest);
+	int n;
+
+	for (n = 0; src[n] != '\0'; n++)
+	{
+		dest[l + n] = src[n];
+	}
+	dest[l + n] = '\0';
+	return (dest);
+}
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+	return (c);
+}
