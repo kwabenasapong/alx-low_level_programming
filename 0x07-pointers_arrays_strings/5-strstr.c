8f26a0305dc9ece6d87c066f338c00c80c5034c1
Kwabena Boadi Sapong <kwabenasapong@users.noreply.github.com>
2022-06-21 05:48:00 +0000

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,15 +9,27 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i,j;
+	int index;
 
-	for (i = 0 ; *needle; i++)
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
 	{
-		for (j = 0; *(haystack + i); j++)
+		index = 0;
+
+		if (haystack[index] == needle[index])
 		{
-			if (*needle == *(haystack + i))
-				return (needle);
+			do {
+				if (needle[index + 1] == '\0')
+					return (haystack);
+
+				index++;
+                } while (haystack[index] == needle[index]);
 		}
-}
+
+		haystack++;
+	}
+
 	return ('\0');
 }
