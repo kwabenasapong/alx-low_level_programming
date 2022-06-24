72117e632c56dd44f3434ac45a752318165b56b4
Kwabena Sapong <kwabenasapong@gmail.com>
2022-06-24 06:06:30 -0700

    Add task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * @argc: number of arguments
+ * @argv: vector arrays in program
+ * Return: Always 0 (success)
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return(0);
+}
