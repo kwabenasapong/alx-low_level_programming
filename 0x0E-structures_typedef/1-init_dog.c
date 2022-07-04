42d5487eb77fbbbe50a63b546d59893e461af83d
kwabenasapong <kwabenasapong@gmail.com>
2022-07-04 21:21:30 +0000

    Add task 1 and dog.h

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -15,12 +15,12 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d = malloc(sizeof(struct dog));
 
 	if (d == NULL)
-		return (NULL);
+		return;
 
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
 
-	return (d);
+	return;
 }
 
