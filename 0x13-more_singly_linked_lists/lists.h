7b5e07b5bf9107703a340a48796080cc9d1ca14a
kwabenasapong <kwabenasapong@gmail.com>
2022-07-19 15:31:22 +0000

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef _LIST_H_
-#define _LIST_H_
+#ifndef _LISTS_H_
+#define _LISTS_H_
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,3 +21,5 @@ typedef struct listint_s
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+
+#endif
