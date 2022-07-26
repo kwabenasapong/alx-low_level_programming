8a964ea79ab75c53d188ddce31aeee9b6f88ec3b
kwabenasapong <kwabenasapong@gmail.com>
2022-07-26 04:22:25 +0000

    add task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to stdout
+ * @filename: the name of the file to be written
+ * @letters: the number of letters to read and print
+ * Return: the number of letters it read and printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, numwrit, numread;
+	char *buf = malloc(sizeof(char) * (letters + 1));
+
+	if (buf == NULL)
+		return (0);
+	if (filename == NULL || letters == 0)
+	{
+		free(buf);
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
+	numread = read(fd, buf, letters);
+	if (numread < 0)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
+	buf[letters] = '\0';
+	numwrit = write(STDOUT_FILENO, buf, numread);
+	if (numwrit <= 0)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
+	close(fd);
+	free(buf);
+	return (numwrit);
+}
