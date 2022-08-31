079b042ba696cc773b706ce9902741ccb9c56121
Kwabena Boadi Sapong <kwabenasapong@users.noreply.github.com>
2022-08-31 15:01:01 +0000

    Create 100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,30 @@
+include "main.h"
+
+/**
+ * is_palindrome -  a function that returns 1
+ * if a string is a palindrome and 0 if no
+ * @s: pointer to char variable
+ *
+ * Return: 1 if true or 0 if false
+ *
+ */
+int is_palindrome(char *s)
+{
+    /* Initialize local variables*/
+    int result, len, i = 0, j = _strlen(s) - 1;
+
+    /* Compare original string to reversed string */
+    while (i < (_strlen(s) - 1))
+    {
+        if (s[i] != s[j])
+        {
+            result = 0;
+            break;
+        }
+        else
+            result = 1;
+    i++;
+    j--;
+    }
+        return (result);
+}
