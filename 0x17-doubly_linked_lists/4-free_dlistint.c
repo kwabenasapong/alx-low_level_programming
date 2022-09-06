39a0d15f20e2f71e0653fcb9c3ded2d4126620d6
kwabenasapong <kwabenasapong@gmail.com>
2022-09-06 06:07:51 +0000

    Add task 4 files

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+
+/**
+ * free_dlistint - function that frees
+ * a dlistint_t list
+ *
+ * @head: node pointer to dlistint_t
+ * Return: nothing
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	temp = head;
+	while (temp->next != NULL)
+	{
+		free(temp);
+		temp = temp->next;
+	}
+}
