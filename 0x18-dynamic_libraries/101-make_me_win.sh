f2f76610e6c7ca6b455af7798f28edc26f8b756d
Kwabena Sapong <kwabenasapong@gmail.com>
2022-09-26 05:30:24 -0700

    code to inject

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export LD_PRELOAD=$PWD/libinjectwin.so
-
+wget -P /tmp https://github.com/kwabenasapong/alx-low_level_programming/blob/main/0x18-dynamic_libraries/win_numbers.o
+export LD_PRELOAD=/tmp/libinjectwin.so
