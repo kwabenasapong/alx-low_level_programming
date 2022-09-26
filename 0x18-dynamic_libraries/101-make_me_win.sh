6a805a38951b658ba5ade8a433d17bb301f3b28a
Kwabena Sapong <kwabenasapong@gmail.com>
2022-09-26 05:40:10 -0700

    101 task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/kwabenasapong/alx-low_level_programming/blob/main/0x18-dynamic_libraries/win_numbers.o
-export LD_PRELOAD=/tmp/libinjectwin.so
+wget -P /tmp https://github.com/kwabenasapong/alx-low_level_programming/blob/main/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
