478f79a20dbaffbf80de20ee02d3f01d598a197f
Kwabena Sapong <kwabenasapong@gmail.com>
2022-11-10 14:45:38 +0000

    Update  task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,12 +9,16 @@ def island_perimeter(grid):
     for y in range(len(grid)):
         for x in range(len(grid[y])):
             if grid[y][x] == 1:
-                if y >= 0 and grid[y - 1][x] == 0:
+                try:
+                    if (y - 1) < 0 or grid[y - 1][x] == 0:
+                        cell_per = cell_per + 1
+                    if (x - 1) < 0 or grid[y][x - 1] == 0:
+                        cell_per = cell_per + 1
+                    if (x + 1) < 0 or grid[y][x + 1] == 0:
+                        cell_per = cell_per + 1
+                    if (y + 1) < 0 or grid[y + 1][x] == 0:
+                        cell_per = cell_per + 1
+                except IndexError:
                     cell_per = cell_per + 1
-                if grid[y][x - 1] == 0:
-                    cell_per = cell_per + 1
-                if grid[y][x + 1] == 0:
-                    cell_per = cell_per + 1
-                if grid[y + 1][x] == 0:
-                    cell_per = cell_per + 1
+                    pass
     return (cell_per)
