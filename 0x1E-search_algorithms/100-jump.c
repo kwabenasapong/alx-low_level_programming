1b3ddf3c14a97676bee848160c4fd1454f95d603
kwabenasapong <kwabenasapong@gmail.com>
2023-01-05 12:48:53 +0000

    Update task 100

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -14,26 +14,24 @@ int jump_search(int *array, size_t size, int value)
 	size_t jump_size;
 	int cur_pos, beg_blk, i;
 
-	jump_size = sqrt(size);
-	cur_pos = 0;
-
 	if (!array)
 		return (-1);
 
-	while (array[cur_pos] < value)
+	jump_size = sqrt(size);
+	cur_pos = 0;
+
+	while (array[cur_pos] < value && cur_pos < (int)size)
 	{
 		printf("Value checked array[%d] = [%d]\n"
 				, cur_pos, array[cur_pos]);
 		cur_pos += (int)jump_size;
-		if (cur_pos > (int)size)
-			return (-1);
 	}
 
 	beg_blk = cur_pos - (int)jump_size;
 
 	printf("Value found between indexes [%d] and [%d]\n"
 			, beg_blk, cur_pos);
-	for (i = beg_blk; i <= cur_pos; i++)
+	for (i = beg_blk; i <= cur_pos && i < (int)size; i++)
 	{
 		printf("Value checked array[%d] = [%d]\n"
 				, i, array[i]);
