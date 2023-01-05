8eed112d8bfff52914be772c8cc81f209667db6f
kwabenasapong <kwabenasapong@gmail.com>
2023-01-05 11:25:03 +0000

    Update task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,11 +1,15 @@
 #include "search_algos.h"
 
 /**
- * linear_search - searches for a value in an array
- * @array: int array parsed
- * @size: size_t size of array
- * @value: int target value
- * Return: first index where value is located
+ * linear_search - searches for a value in an array.
+ *
+ * @array: int array parsed.
+ * @size: size_t size of array.
+ * @value: int target value.
+ *
+ * Return: first index where value is located.
+ *
+ * Description: Prints a value everytime it is compared in the array.
  */
 int linear_search(int *array, size_t size, int value)
 {
